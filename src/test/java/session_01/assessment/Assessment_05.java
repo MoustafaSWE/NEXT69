7d987e90843ebef6e1b790f2bf337565b9730743
MoustafaSWE <moustafa.mo.ismail@gmail.com>
2024-09-08 20:03:29 +0300

    add assessment number 5

--- src/test/java/session_01/assessment/Assessment_05.java
+++ src/test/java/session_01/assessment/Assessment_05.java
@@ -1,5 +1,7 @@
 package session_01.assessment;
 
+import java.util.Scanner;
+
 public class Assessment_05 {
 
     /*
@@ -13,13 +15,38 @@ public class Assessment_05 {
         and your sim-card is vodafone
      */
     public static void main(String[] args) {
+        while (true) {
+            try {
+                // we asked user to enter mobile number
+                System.out.println("Please, Enter your mobile number?");
+                // create object from Scanner Class
+                Scanner scanner = new Scanner(System.in);
+                // Store text, variable enterText
+                String enterText = scanner.nextLine();  // "01052636"
+                int number = Integer.parseInt(enterText);
 
-        String numberAsText = "010000";
-        String firstThreeInt = numberAsText.substring(0,3);
+                System.out.println("Your mobile Number is: 0" + number);
 
-        int numberFromText = Integer.parseInt(numberAsText);
+                String firstThreeNumbers = enterText.substring(0,3);
 
-        System.out.println("0"+numberFromText);
-        System.out.println(firstThreeInt);
+                switch (firstThreeNumbers){
+                    case "010":
+                        System.out.println("Your sim-card is vodafone");
+                        break;
+                    case "011":
+                        System.out.println("Your sim-card is etisalat");
+                        break;
+                    case "012":
+                        System.out.println("Your sim-card is orange");
+                        break;
+                    default:
+                        System.out.println("You entered Wrong mobile Number");
+                        break;
+                }
+                break;
+            } catch (Exception e) {
+                System.out.println("Please, Enter valid mobile number");
+            }
+        }
     }
 }
