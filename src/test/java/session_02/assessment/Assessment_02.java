9156d22311f2b2a31d5106cff5ac08855cc657a3
Mostafa.ismail <mostafa.ismail@cyshield.com>
2024-09-08 15:18:49 +0300

    session 2

---
+++ src/test/java/session_02/assessment/Assessment_02.java
@@ -0,0 +1,24 @@
+package session_02.assessment;
+
+public class Assessment_02 {
+
+    /*
+
+    Write a simple calculator program that:
+
+        Asks the user to input two numbers.
+        Asks the user to select an operation (+, -, *, /).
+        Based on the selected operation, perform the calculation and print the result.
+        Use a switch statement to handle the operation selection, and include a try-catch block to handle division by zero errors.
+
+    Requirements:
+
+        Use a switch statement to decide which operation to perform.
+        The program should handle invalid operators by displaying an error message.
+        Catch and handle any arithmetic exceptions (like division by zero).
+
+ */
+    public static void main(String[] args) {
+
+    }
+}
