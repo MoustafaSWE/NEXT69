e7e2e0c996f935491e57782a75d765683b7a16f0
MoustafaSWE <moustafa.mo.ismail@gmail.com>
2024-09-15 03:27:13 +0300

    add session 4

--- src/test/java/session_03/firstAutomationScript/FirstAutomationScript.java
+++ src/test/java/session_03/firstAutomationScript/FirstAutomationScript.java
@@ -1,9 +1,32 @@
 package session_03.firstAutomationScript;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
 public class FirstAutomationScript {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws InterruptedException {
+
+        // init. chrome driver
+        WebDriver driver;
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--incognito");  // Search value -> incognito
+
+        driver = new ChromeDriver(options);
+
+        // Open browser and navigate to google
+        driver.get("https://www.google.com.eg/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+        Thread.sleep(20000);
+
+        driver.quit();
+
     }
 
 
---
+++ src/test/java/session_04/seleniumWebCommands/T03_findelements.java
@@ -0,0 +1,20 @@
+package session_04.seleniumWebCommands;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.v85.indexeddb.model.Key;
+
+import java.time.Duration;
+
+public class T03_findelements {
+
+    //    Don't apply Thread.sleep -> it is for you to see the execution ; remove it once you learn the script's purpose
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
