f406b54f6f3a7eaef3873c0ef905f71806e6cbe2
MoustafaSWE <moustafa.mo.ismail@gmail.com>
2024-09-25 21:51:08 +0300

    update session 6

--- src/test/java/session_06/assessment/Assessment_01.java
+++ src/test/java/session_06/assessment/Assessment_01.java
@@ -1,10 +1,13 @@
 package session_06.assessment;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.Color;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
 
@@ -29,11 +32,41 @@ public class Assessment_01 {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 
+        //1. Open URL: https://www.saucedemo.com/v1/
         driver.navigate().to("https://www.saucedemo.com/v1/");
     }
 
     @Test
     public void testCase01 (){
+        SoftAssert softAssert = new SoftAssert();
+
+        //2. Enter Valid username: standard_user
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        //3. Enter invalid password: standard_user
+        driver.findElement(By.id("password")).sendKeys("standard_user");
+        //4. Click on Login button
+        driver.findElement(By.id("login-button")).click();
+
+        //1. Assert that this error message is displayed: "Username and password do not match any user in this service"
+        String actualText = driver.findElement(By.cssSelector("h3[data-test=\"error\"]")).getText();
+        String expectedText = "Username and password do not match any user in this service";
+
+        softAssert.assertEquals(actualText,expectedText,"Error message does not meet '" + expectedText + "'");
+
+        //2. Assert that logo displayed beside error message, it's color match Hex Color: #ff0000
+        String actualRGBColor = driver.findElement(By.className("error-button")).getCssValue("color");
+        String actualHexColor = Color.fromString(actualRGBColor).asHex();
+        String expectedHexColor = "#ff0000";
+
+        softAssert.assertEquals(actualHexColor,expectedHexColor,"Logo displayed color does not match expected: "+ expectedHexColor);
+
+        //3. Assert that you are not on this URL: https://www.saucedemo.com/v1/inventory.html
+        String actualURl = driver.getCurrentUrl();
+        String expectedURL = "https://www.saucedemo.com/v1/inventory.html";
+
+        softAssert.assertFalse(actualURl.equals(expectedURL),"user navigated to url: " + actualURl);
+
+        softAssert.assertAll();
     }
 
     @AfterMethod
